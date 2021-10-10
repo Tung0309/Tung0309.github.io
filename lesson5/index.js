038bb6efbfcd04c0060e54739b0bacb3205d5300
Tung0309 <88965691+Tung0309@users.noreply.github.com>
2021-10-10 16:18:19 +0700

    :D ?

---
+++ lesson5/index.js
@@ -0,0 +1,21 @@
+//let a = 30
+//if (a<18){
+    //console.log("you are kid")
+//}
+//else if(a<30){
+    //console.log("may la thang trung nien")
+//}
+//else if(a>30){
+    //console.log("ong/ba gia")
+//}
+
+function hoatdongbuoitoi(thu){
+    console.log("thứ" + thu);
+    console.log("ăn tối");
+    console.log("học bài");
+    console.log("đánh răng")
+    console.log("haha ko bật mic")
+}
+hoatdongbuoitoi(2)
+hoatdongbuoitoi(3)
+hoatdongbuoitoi(4)
---
+++ new/dom.js
@@ -0,0 +1,10 @@
+let domTung = document.getElementById("a")
+console.log(domTung);
+console.dir(domTung)
+function offlight(){
+    domTung.src="https://www.w3schools.com/js/pic_bulboff.gif"
+}
+
+function onlight(){
+    domTung.src="https://www.w3schools.com/js/pic_bulbon.gif"
+}
